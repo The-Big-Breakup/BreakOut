4935c4ceed5e8d8b65e184b57494171bf642ae8f
Christian Jäderberg <christianjaderberg@gmail.com>
2019-11-11 22:29:11 +0100

    Javadoc
    Minor Javadoc

--- BreakOut/app/src/main/java/com/thebigbreakup/breakout/sprites/BrickSprite.java
+++ BreakOut/app/src/main/java/com/thebigbreakup/breakout/sprites/BrickSprite.java
@@ -3,13 +3,22 @@ package com.thebigbreakup.breakout.sprites;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
+/**
+ * A class that represents a Brick in the game
+ */
 public class BrickSprite {
 
     private Bitmap image;
-    private int xPos, yPos;
-    private int rewardPoints;
+    private int xPos, yPos, rewardPoints;
     private boolean destroyed = false;
 
+    /**
+     * Constructor method for the Brick class
+     * @param image A Bitmap value to use as the graphic for the Brick object
+     * @param xPos The x-position of the Brick object
+     * @param yPos The y-position of the Brick object
+     * @param rewardPoints The amount of points a Player gets when a brick is destroyed
+     */
     public BrickSprite(Bitmap image, int xPos, int yPos, int rewardPoints) {
         this.image = image;
         this.xPos = xPos;
