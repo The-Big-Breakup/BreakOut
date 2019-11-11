5083e3710efda0d6c17833ba55e99f86dd366a86
Christian Jäderberg <christianjaderberg@gmail.com>
2019-11-11 20:18:09 +0100

    Structure
    Basic brick class structure, WIP

--- BreakOut/app/src/main/java/com/thebigbreakup/breakout/sprites/BrickSprite.java
+++ BreakOut/app/src/main/java/com/thebigbreakup/breakout/sprites/BrickSprite.java
@@ -1,6 +1,25 @@
 package com.thebigbreakup.breakout.sprites;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
 public class BrickSprite {
+
+    private Bitmap image;
+    private int xPos, yPos;
+    private int rewardPoints;
+    private boolean destroyed = false;
+
+    public BrickSprite(Bitmap image, int rewardPoints, int xPos, int yPos) {
+        this.image = image;
+        this.rewardPoints = rewardPoints;
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(this.image, this.xPos, this.yPos, null);
+    }
 
     //TODO: Add Values(DestroyValuePoints etc) and size
     //TODO: Add collision event (Remove the block etc.)
