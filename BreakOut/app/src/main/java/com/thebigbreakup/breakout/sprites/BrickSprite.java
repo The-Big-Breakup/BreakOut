eaaab5e9f22ef184a0e1a44493bd64d17bc0c23f
Christian Jäderberg <christianjaderberg@gmail.com>
2019-11-11 21:11:05 +0100

    Getters/setters
    Improvements and getters/setters

--- BreakOut/app/src/main/java/com/thebigbreakup/breakout/sprites/BrickSprite.java
+++ BreakOut/app/src/main/java/com/thebigbreakup/breakout/sprites/BrickSprite.java
@@ -10,15 +10,54 @@ public class BrickSprite {
     private int rewardPoints;
     private boolean destroyed = false;
 
-    public BrickSprite(Bitmap image, int rewardPoints, int xPos, int yPos) {
+    public BrickSprite(Bitmap image, int xPos, int yPos, int rewardPoints) {
         this.image = image;
-        this.rewardPoints = rewardPoints;
         this.xPos = xPos;
+        this.yPos = yPos;
+        this.rewardPoints = rewardPoints;
+    }
+
+    // If the Brick is not destroyed - draw the Brick
+    public void draw(Canvas canvas) {
+        if(!destroyed) {
+            canvas.drawBitmap(this.image, this.xPos, this.yPos, null);
+        }
+    }
+
+    public void destroy() {
+        this.destroyed = true;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public int getxPos() {
+        return xPos;
+    }
+
+    public void setxPos(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public void setyPos(int yPos) {
         this.yPos = yPos;
     }
 
-    public void draw(Canvas canvas) {
-        canvas.drawBitmap(this.image, this.xPos, this.yPos, null);
+    public int getRewardPoints() {
+        return rewardPoints;
+    }
+
+    public void setRewardPoints(int rewardPoints) {
+        this.rewardPoints = rewardPoints;
+    }
+
+    public boolean isDestroyed() {
+        return destroyed;
     }
 
     //TODO: Add Values(DestroyValuePoints etc) and size
