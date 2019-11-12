ca977a25fdc735a02c8cfc91e46624da46037e7f
CyrilleKolle <ngidekollecyrille@gmail.com>
2019-11-12 15:15:35 +0100

    paddle moves right and left

--- BreakOut/app/src/main/java/com/thebigbreakup/breakout/sprites/PaddleSprite.java
+++ BreakOut/app/src/main/java/com/thebigbreakup/breakout/sprites/PaddleSprite.java
@@ -1,6 +1,10 @@
 package com.thebigbreakup.breakout.sprites;
 
+import android.graphics.RectF;
+
 public class PaddleSprite {
+
+
 
     //TODO: set values to paddle (Width, height etc)
     //TODO: make a shape and load it here to set design
@@ -8,8 +12,46 @@ public class PaddleSprite {
     //TODO: make the paddle return a value depending on where we hit it
 
     //TODO: Move the paddle after the touchevent
+private RectF rect;
+private float length;
+private float height;
+private float x;
+private float y;
+private float paddleSpeed;
+public final int stopped = 0;
+public final int left=1;
+public final int right = 1;
 
+private int paddleMoving = stopped;
 
+public PaddleSprite(int screenX, int screenY){
+    length = 130;
+    height = 20;
 
+    x = screenX/2;
+    y = screenY - 20;
+
+    rect = new RectF(x, y, x+length, y+height);
+
+    paddleSpeed = 350;
+
+}
+public RectF getRect(){
+    return rect;
+}
+public void setMovementState(int state){
+    paddleMoving = state;
+}
+public void update (long fps){
+     if (paddleMoving==left){
+         x = x - paddleSpeed/fps;
+     }
+     if (paddleMoving==right){
+         x = x + paddleSpeed/fps;
+     }
+
+     rect.left = x;
+     rect.right = x + length;
+}
 
 }
