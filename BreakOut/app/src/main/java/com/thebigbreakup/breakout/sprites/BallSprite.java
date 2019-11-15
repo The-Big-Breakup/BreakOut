2d24eed6be27a9b6a77d94f32bb3204ffac4f3c9
Timmy <timmy@iteda.se>
2019-11-15 15:00:39 +0100

    Collisions added
    Added collision logic to ball

--- BreakOut/app/src/main/java/com/thebigbreakup/breakout/sprites/BallSprite.java
+++ BreakOut/app/src/main/java/com/thebigbreakup/breakout/sprites/BallSprite.java
@@ -3,6 +3,7 @@ package com.thebigbreakup.breakout.sprites;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 
 public class BallSprite {
 
@@ -11,9 +12,9 @@ public class BallSprite {
     private int xPosition = screenWidth/2; //change to canvas as grid, and should initially be returned from the paddle on first tocuh
     private int yPosition = screenHeight/40;
     private boolean keepxDirection = true;
+    private boolean keepyDirection = true;
     private Bitmap image;
-
-
+    private Canvas canvas;
 
     public void move(int x, int y) { //same x and y values must be fed to 'move' continously until brick or paddle changes them by adding/subtracting to a sent variable
 
@@ -21,14 +22,24 @@ public class BallSprite {
             x *= -1;
         }
 
+        if(keepyDirection = false){
+            x *= -1;
+        }
+
         while((screenWidth > xPosition && xPosition > 0 || x != 0)){ //detects collision
             if (x > 0){
                 xPosition++;
                 x--;
+                if (isFilled(xPosition)) {
+                    collideX();
+                }
             }
             else if (x < 0){
                 xPosition--;
                 x++;
+                if (isFilled(xPosition)) {
+                    collideX();
+                }
             }
         }
 
@@ -40,28 +51,49 @@ public class BallSprite {
             keepxDirection = true;
         }
 
-        while((screenHeight > yPosition && yPosition > 0 || y != 0) {
-            if (y > 0){
+        while((screenHeight > yPosition && yPosition > 0 || y != 0)) {
+            if (y < 0){
                 yPosition++;
                 y--;
+                if (isFilled(yPosition)) {
+                    collideX();
+                }
             }
-            else if (y < 0){// ändra så att undre delen av skärmen triggar 'lose'
+            else if (y > 0){// ändra så att undre delen av skärmen triggar 'lose'
                 yPosition--;
                 y++;
+                if (isFilled(yPosition)) {
+                    collideX();
+                }
             }
         }
 
         yPosition += y * -1;
 
+        if(x != 0){
+            keepyDirection = false;
+        }else {
+            keepyDirection = true;
+        }
 
-        drawBall();//här ska canvasen uppdateras
+        drawBall(canvas);//här ska canvasen uppdateras
     }
 
     public void drawBall(Canvas canvas) {
         canvas.drawBitmap(this.image, xPosition, yPosition, null);
     }
 
+    private boolean collideX() {
+        return keepxDirection = !keepxDirection;
+    }
 
+    private boolean collideY() {
+        return keepyDirection = !keepyDirection;
+    }
+
+    private static boolean isFilled(int pixel) {
+        return pixel != Color.TRANSPARENT;
+    }
 
     // TODO: Make the variables for the ball (Size, Speed, shape etc.)
 
