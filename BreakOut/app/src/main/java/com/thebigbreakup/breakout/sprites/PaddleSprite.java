a0eeeb4f86364048e3c39a16ca9fc230bb0b1509
CyrilleKolle <ngidekollecyrille@gmail.com>
2019-11-18 11:11:43 +0100

    paddle moves right and left updated

--- BreakOut/app/src/main/java/com/thebigbreakup/breakout/sprites/PaddleSprite.java
+++ BreakOut/app/src/main/java/com/thebigbreakup/breakout/sprites/PaddleSprite.java
@@ -1,5 +1,7 @@
 package com.thebigbreakup.breakout.sprites;
 
+import android.graphics.Bitmap;
+import android.graphics.Rect;
 import android.graphics.RectF;
 
 public class PaddleSprite {
@@ -8,41 +10,40 @@ public class PaddleSprite {
 
     //TODO: set values to paddle (Width, height etc)
     //TODO: make a shape and load it here to set design
-
+    //TODO: Move the paddle after the touchevent
     //TODO: make the paddle return a value depending on where we hit it
 
-    //TODO: Move the paddle after the touchevent
+public final int stopped = 0;
+public final int left=1;
+public final int right = 2;
 private RectF rect;
-private float length;
+private float width;
 private float height;
 private float x;
 private float y;
 private float paddleSpeed;
-public final int stopped = 0;
-public final int left=1;
-public final int right = 1;
-
 private int paddleMoving = stopped;
 
-public PaddleSprite(int screenX, int screenY){
-    length = 130;
-    height = 20;
+public PaddleSprite(int screenX, int screenY, Bitmap bmPlayer){
+    this.width = bmPlayer.getWidth();
+    this.height = bmPlayer.getHeight();
+    this.x = (screenX/2)-(width/2);
+    this.y = screenY-height;
+    //height = 20;
 
-    x = screenX/2;
-    y = screenY - 20;
+   // x = screenX/2;
+    //y = screenY - 20;
 
-    rect = new RectF(x, y, x+length, y+height);
+    this.rect = new RectF(x, y, x+width, y+height);
 
-    paddleSpeed = 350;
+    this.paddleSpeed = 350;
 
 }
-public RectF getRect(){
-    return rect;
-}
+
 public void setMovementState(int state){
     paddleMoving = state;
 }
-public void update (long fps){
+public void update (long fps, int screenX){
      if (paddleMoving==left){
          x = x - paddleSpeed/fps;
      }
@@ -51,7 +52,24 @@ public void update (long fps){
      }
 
      rect.left = x;
-     rect.right = x + length;
+     rect.right = x + width;
 }
+    public RectF getRect(){
+        return rect;
+    }
+    public float getWidth() {
+        return width;
+    }
 
+    public float getHeight() {
+        return height;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
 }
