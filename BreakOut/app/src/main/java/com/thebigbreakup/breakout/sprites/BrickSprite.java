994bc493663a63a744996438a5cd1ae99b826873
Christian Jäderberg <christianjaderberg@gmail.com>
2019-11-19 10:15:33 +0100

    Dynamic width/height
    Dynamically set width/height depending on screen dimensions

--- BreakOut/app/src/main/java/com/thebigbreakup/breakout/sprites/BrickSprite.java
+++ BreakOut/app/src/main/java/com/thebigbreakup/breakout/sprites/BrickSprite.java
@@ -1,5 +1,6 @@
 package com.thebigbreakup.breakout.sprites;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
@@ -11,22 +12,29 @@ public class BrickSprite {
     private Bitmap image;
     private Canvas canvas;
     private int xPos, yPos, width, height, rewardPoints;
+    private int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+    private int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
     private boolean destroyed = false;
 
     /**
-     * Constructor method for the Brick class
+     * Constructor method for the Brick class.
+     * Dynamically generates the bricks width/height from the screen dimensions
+     * and scales the Bitmap image to these values.
      * @param image A Bitmap value to use as the graphic for the Brick object
      * @param xPos The x-position of the Brick object
      * @param yPos The y-position of the Brick object
      * @param rewardPoints The amount of points a Player gets when a brick is destroyed
      */
     public BrickSprite(Bitmap image, int xPos, int yPos, int rewardPoints) {
-        this.image = image;
         this.xPos = xPos;
         this.yPos = yPos;
-        this.width = image.getWidth();
-        this.height = image.getHeight();
+        this.width = screenWidth / 10; // set brick width depending on screenwidth
+        this.height = screenHeight / 40; // set brick height depending on screenheight
         this.rewardPoints = rewardPoints;
+
+        // scale the bitmap image to the dynamically generated width/height
+        this.image = Bitmap.createScaledBitmap(image, this.width, this.height, false);
+        this.image.recycle(); // for better memory performance
     }
 
     // If the Brick is not destroyed - draw the Brick
