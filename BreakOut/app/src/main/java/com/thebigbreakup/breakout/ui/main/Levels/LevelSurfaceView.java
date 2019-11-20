6e2274a873b146818fe0bd4588f43ee54306a0c6
Timmy <timmy@iteda.se>
2019-11-20 23:06:45 +0100

    LevelSurfaceView cleanup
    Cleaned

--- BreakOut/app/src/main/java/com/thebigbreakup/breakout/ui/main/Levels/LevelSurfaceView.java
+++ BreakOut/app/src/main/java/com/thebigbreakup/breakout/ui/main/Levels/LevelSurfaceView.java
@@ -3,15 +3,17 @@ package com.thebigbreakup.breakout.ui.main.Levels;
 
 import android.content.Context;
 
-import android.content.res.Resources;
+//import android.content.res.Resources;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import com.thebigbreakup.breakout.GameThread;
 import com.thebigbreakup.breakout.R;
 import com.thebigbreakup.breakout.sprites.BallSprite;
+import com.thebigbreakup.breakout.sprites.BrickSprite;
 import com.thebigbreakup.breakout.sprites.PaddleSprite;
 
 public class LevelSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
@@ -19,10 +21,11 @@ public class LevelSurfaceView extends SurfaceView implements SurfaceHolder.Callb
     private GameThread thread;
     private BallSprite ballSprite;
     private PaddleSprite paddleSprite;
-    private int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
-    private int screenHight = Resources.getSystem().getDisplayMetrics().heightPixels;
+    //private int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+    //private int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
     private int speedX = 2;
     private int speedY = 1;
+    private BrickSprite[] bricks;
 
     public LevelSurfaceView(Context context) {
         super(context);
