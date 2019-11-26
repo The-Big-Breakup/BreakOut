ebdbfa211341f96246f87baffd37e23da4032de9
Roman <Divkovicroman@gmail.com>
2019-11-26 13:47:53 +0100

    Fixing speed and size on paddle, deleting code
    Faster paddle, smaller paddle and deleting unnecessary code

--- BreakOut/app/src/main/java/com/thebigbreakup/breakout/sprites/PaddleSprite.java
+++ BreakOut/app/src/main/java/com/thebigbreakup/breakout/sprites/PaddleSprite.java
@@ -13,7 +13,7 @@ import android.view.View;
 
 import com.thebigbreakup.breakout.R;
 
-public class PaddleSprite implements View.OnTouchListener {
+public class PaddleSprite {
 
 
 
@@ -36,23 +36,14 @@ private Canvas canvas;
 private Bitmap bitmap;
 private int posY, posX;
 private Rect bounds;
-private int paddleScaleFactor = 5;
-
-
-
-
+private int paddleScaleFactor = 10;
 private int touchedPixel;
 private int screenwidth = Resources.getSystem().getDisplayMetrics().widthPixels;
 private int paddleSide =screenwidth/paddleScaleFactor;
 private int paddleLength = screenwidth/5;
 private int paddlePosition;
 private int leftSide = paddlePosition - (screenwidth/paddleLength);
-private int c0 = paddleLength - paddlePosition;
-private int c1 = paddleLength - paddlePosition + 1 * paddleLength/5;
-private int c2 = paddleLength - paddlePosition + 2 * paddleLength/5;
-private int c3 = paddleLength - paddlePosition + 3 * paddleLength/5;
-private int c4 = paddleLength - paddlePosition + 4 * paddleLength/5;
-private int c5 = paddleLength - paddlePosition + 5 * paddleLength/5;
+
 
 
 public int updateTouchMove() {
@@ -68,26 +59,6 @@ public int updateTouchMove() {
     return paddlePosition;
 }
 
-public int paddleCollision(int x,int y) {
-    if (x >= c0 && x <= c1) {
-        x = -2;
-        return y *= -1;
-    } else if(x == c1 && x<= c2) {
-        x = -1;
-        return y *= -1;
-    } else if(x == c2 && x<= c3) {
-        x = 0;
-        return y *= -1;
-    } else if(x == c3 && x<= c4) {
-        x = 1;
-        return y *= -1;
-    } else if(x == c4 && x<= c5) {
-        x = 2;
-        return y *= -1;
-    }else {
-        return x;
-    }
-}
 
 
 public PaddleSprite(int screenX, int screenY, Bitmap bmPlayer){
@@ -111,7 +82,7 @@ public PaddleSprite(int screenX, int screenY, Bitmap bmPlayer){
 
     this.rect = new Rect(x, y, x+width, y+height);
 
-    this.paddleSpeed = 800;
+    this.paddleSpeed = 2500;
 
 
 }
@@ -124,66 +95,40 @@ public PaddleSprite(int screenX, int screenY, Bitmap bmPlayer){
     paddleMoving = state;
     }
     public void update (int fps, int screenX){
-     if (paddleMoving==left){
+     if (paddleMoving == left) {
          if(x>=0)
          x = x - paddleSpeed/fps;
 
          else
              x=0;
      }
-     if (paddleMoving==right){
-         if(x+width<=screenX)
+     if (paddleMoving == right) {
+
+         if(x + width <= screenX)
          x = x + paddleSpeed/fps;
          else
              x=0;
      }
-     /*if(rect.left<0){
-         x=0;
-     }
-     if(rect.right>screenX){
-         x=(int)(screenX-(rect.right-rect.left));
-     }*/
      rect.left = x;
      rect.right = x + width;
     }
     public Rect getRect(){
         return rect;
     }
-    public float getWidth() {
+    public int getWidth() {
         return width;
     }
 
-    public float getHeight() {
+    public int getHeight() {
         return height;
     }
 
-    public float getX() {
+    public int getX() {
         return x;
     }
 
-    public float getY() {
+    public int getY() {
         return y;
-    }
-    public void movmentX(int speedx) {
-
-    }
-    public void movmentY(int speedy){
-
-    }
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-
-        switch(event.getAction()& MotionEvent.ACTION_MASK){
-            case MotionEvent.ACTION_DOWN:
-
-                break;
-
-            case MotionEvent.ACTION_UP:
-                break;
-
-        }
-
-        return true;
     }
     public void setImage(Bitmap image) {
         this.bitmap = image;
