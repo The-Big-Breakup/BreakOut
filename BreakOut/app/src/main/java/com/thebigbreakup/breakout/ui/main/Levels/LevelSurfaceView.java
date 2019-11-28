9e2483c3016eeea811f08f0ce180aad4fae1514f
Cyrille <ngidekollecyrille@gmail.com>
2019-11-28 13:59:56 +0100

    winFragment in levelSurfaceView

--- BreakOut/app/src/main/java/com/thebigbreakup/breakout/ui/main/Levels/LevelSurfaceView.java
+++ BreakOut/app/src/main/java/com/thebigbreakup/breakout/ui/main/Levels/LevelSurfaceView.java
@@ -1,6 +1,7 @@
 package com.thebigbreakup.breakout.ui.main.Levels;
 
 
+import android.app.Activity;
 import android.content.Context;
 
 //import android.content.res.Resources;
@@ -9,16 +10,28 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.thebigbreakup.breakout.GameThread;
 import com.thebigbreakup.breakout.R;
 import com.thebigbreakup.breakout.sprites.BallSprite;
 import com.thebigbreakup.breakout.sprites.BrickSprite;
 import com.thebigbreakup.breakout.sprites.PaddleSprite;
+import com.thebigbreakup.breakout.ui.main.GameResults.WinFragment;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class LevelSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
 
@@ -31,6 +44,8 @@ public class LevelSurfaceView extends SurfaceView implements SurfaceHolder.Callb
     private int speedY = 11;
     private int paddleYPosition = (int)Math.round(screenHeight*0.9);
     private BrickSprite[] bricks;
+    private int bricksDestroyed = 0;
+
 
     public LevelSurfaceView(Context context) {
         super(context);
@@ -145,8 +160,147 @@ public class LevelSurfaceView extends SurfaceView implements SurfaceHolder.Callb
 
             if (ballBounds.intersect(brickBounds) || ballBounds.contains(brickBounds) || brickBounds.contains(ballBounds)) {
                 brick.destroy();
+                bricksDestroyed++;
                 return true;
             }
+        }
+        if(bricksDestroyed >= bricks.length){
+
+            Fragment winFragment;
+            FragmentManager fm = new FragmentManager() {
+                @NonNull
+                @Override
+                public FragmentTransaction beginTransaction() {
+                    return null;
+                }
+
+                @Override
+                public boolean executePendingTransactions() {
+                    return false;
+                }
+
+                @Nullable
+                @Override
+                public Fragment findFragmentById(int id) {
+                    return null;
+                }
+
+                @Nullable
+                @Override
+                public Fragment findFragmentByTag(@Nullable String tag) {
+                    return null;
+                }
+
+                @Override
+                public void popBackStack() {
+
+                }
+
+                @Override
+                public boolean popBackStackImmediate() {
+                    return false;
+                }
+
+                @Override
+                public void popBackStack(@Nullable String name, int flags) {
+
+                }
+
+                @Override
+                public boolean popBackStackImmediate(@Nullable String name, int flags) {
+                    return false;
+                }
+
+                @Override
+                public void popBackStack(int id, int flags) {
+
+                }
+
+                @Override
+                public boolean popBackStackImmediate(int id, int flags) {
+                    return false;
+                }
+
+                @Override
+                public int getBackStackEntryCount() {
+                    return 0;
+                }
+
+                @NonNull
+                @Override
+                public BackStackEntry getBackStackEntryAt(int index) {
+                    return null;
+                }
+
+                @Override
+                public void addOnBackStackChangedListener(@NonNull OnBackStackChangedListener listener) {
+
+                }
+
+                @Override
+                public void removeOnBackStackChangedListener(@NonNull OnBackStackChangedListener listener) {
+
+                }
+
+                @Override
+                public void putFragment(@NonNull Bundle bundle, @NonNull String key, @NonNull Fragment fragment) {
+
+                }
+
+                @Nullable
+                @Override
+                public Fragment getFragment(@NonNull Bundle bundle, @NonNull String key) {
+                    return null;
+                }
+
+                @NonNull
+                @Override
+                public List<Fragment> getFragments() {
+                    return null;
+                }
+
+                @Nullable
+                @Override
+                public Fragment.SavedState saveFragmentInstanceState(@NonNull Fragment f) {
+                    return null;
+                }
+
+                @Override
+                public boolean isDestroyed() {
+                    return false;
+                }
+
+                @Override
+                public void registerFragmentLifecycleCallbacks(@NonNull FragmentLifecycleCallbacks cb, boolean recursive) {
+
+                }
+
+                @Override
+                public void unregisterFragmentLifecycleCallbacks(@NonNull FragmentLifecycleCallbacks cb) {
+
+                }
+
+                @Nullable
+                @Override
+                public Fragment getPrimaryNavigationFragment() {
+                    return null;
+                }
+
+                @Override
+                public void dump(@NonNull String prefix, @Nullable FileDescriptor fd, @NonNull PrintWriter writer, @Nullable String[] args) {
+
+                }
+
+                @Override
+                public boolean isStateSaved() {
+                    return false;
+                }
+            };
+            fm.beginTransaction().replace(R.id.fragmentContainerID, new WinFragment()).commit();
+            //winFragment = fm.findFragmentById(R.id.fragmentContainerID);
+            //fm.beginTransaction().add(R.id.fragmentContainerID, winFragment).commit();
+            //trigga win
+
         }
 
         return false;
