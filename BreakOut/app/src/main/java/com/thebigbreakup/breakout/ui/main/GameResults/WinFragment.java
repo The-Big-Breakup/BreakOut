7a536e331835a79a11ec0bf807229e665685b353
Roman <Divkovicroman@gmail.com>
2019-11-29 19:24:12 +0100

    Added functions to the buttons to exit and play again
    Added code that ends and restarts the game

--- BreakOut/app/src/main/java/com/thebigbreakup/breakout/ui/main/GameResults/WinFragment.java
+++ BreakOut/app/src/main/java/com/thebigbreakup/breakout/ui/main/GameResults/WinFragment.java
@@ -3,6 +3,8 @@ package com.thebigbreakup.breakout.ui.main.GameResults;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.thebigbreakup.breakout.R;
+import com.thebigbreakup.breakout.ui.main.MainMenuFragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,14 +32,31 @@ public class WinFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       // return inflater.inflate(R.layout.fragment_win, container, false);
-        View v = inflater.inflate(R.layout.fragment_lose, container, false);
+        return inflater.inflate(R.layout.fragment_win, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
         Exit = getView().findViewById(R.id.Exit);
         Game = getView().findViewById(R.id.Game);
 
+        Game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.fragmentContainerID, new MainMenuFragment()).commit();
+            }
+        });
+
+        Exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
 
 
-        return v;
     }
 
 }
