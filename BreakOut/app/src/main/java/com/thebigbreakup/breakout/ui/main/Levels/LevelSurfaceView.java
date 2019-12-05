6f724db9b641ee3c793cb01880c98c18db95be0e
Eric <ericj_@hotmail.com>
2019-12-05 11:36:14 +0100

    Disable Paddle
    Disables paddle movement after loss/win

--- BreakOut/app/src/main/java/com/thebigbreakup/breakout/ui/main/Levels/LevelSurfaceView.java
+++ BreakOut/app/src/main/java/com/thebigbreakup/breakout/ui/main/Levels/LevelSurfaceView.java
@@ -143,7 +143,7 @@ public class LevelSurfaceView extends SurfaceView implements SurfaceHolder.Callb
             ballSprite.setxPosition(paddleSprite.getX()  + paddleSprite.getWidth() / 2);
         }
 
-        if (paddleMotion != null) {
+        if (paddleMotion != null && !win &&!lose) {
             paddleSprite.update(paddleMotion);
         }
     }
